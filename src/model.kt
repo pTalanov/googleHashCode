a04f755d237ada7a4fd61c92e50ca793b1c2d649
Pavel V. Talanov <talanov.pavel@gmail.com>
2018-03-01 18:57:40 +0100

    Model draft

---
+++ src/model.kt
@@ -0,0 +1,16 @@
+class Model(
+        val map: Map,
+        val fleetSize: Int,
+        val rides: List<Ride>,
+        val perRideBonus: Int,
+        val numberOfSteps: Long
+)
+
+data class Map(val verticalSize: Int, val horizontalSize: Int)
+data class Point(val x: Int, val y: Int)
+data class Ride(
+    val startPoint: Point,
+    val endPoint: Point,
+    val earliestStart: Long,
+    val latestFinish: Long
+)
